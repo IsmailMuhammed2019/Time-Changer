8d7074bbbd439fc4f808da5035d6792c67ace889
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2020-07-02 14:10:39 +0100

    set background for different time set

--- js/main.js
+++ js/main.js
@@ -37,19 +37,21 @@ const time = document.querySelector('#time'),
 
         if(hour < 12){
             //Morning
-            document.body.style.backgroundImage = "URL('../img/morning.jpg')";
+            document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
             greeting.textContent = 'Good Morning';
         }else if (hour < 18){
             // Afternoon
-            document.body.style.backgroundImage = "URL('../img/afternoon.jpg')";
+            document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
             greeting.textContent = 'Good Afternoon';
         }else {
             //Evening
-            document.body.style.backgroundImage = "URL('../img/night.jpg')";
+            document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
             greeting.textContent = 'Good Evening';
+            document.body.style.color = 'white';
         }
     }
 
     //Run
 
     showTime();
+    setBgGreet();
