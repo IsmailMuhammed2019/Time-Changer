3d3e3848f48e6eadd3efa68fe35456e9cf95fdb3
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2020-07-02 13:33:28 +0100

    finished setting time and adjusting the day with background

--- js/main.js
+++ js/main.js
@@ -20,9 +20,34 @@ const time = document.querySelector('#time'),
         hour = hour % 12 || 12;
 
         //Output Time
-        time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
+        time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
         setTimeout(showTime, 1000);
+    }
+
+    //Add Zero
+    function addZero(n){
+        return (parseInt(n, 10) < 10 ? '0' : '') + n;
+    }
+
+    // Set Background and Greeting
+    function setBgGreet(){
+        let today = new Date(),
+        hour = today.getHours();
+
+        if(hour < 12){
+            //Morning
+            document.body.style.backgroundImage = "URL('../img/morning.jpg')";
+            greeting.textContent = 'Good Morning';
+        }else if (hour < 18){
+            // Afternoon
+            document.body.style.backgroundImage = "URL('../img/afternoon.jpg')";
+            greeting.textContent = 'Good Afternoon';
+        }else {
+            //Evening
+            document.body.style.backgroundImage = "URL('../img/night.jpg')";
+            greeting.textContent = 'Good Evening';
+        }
     }
 
     //Run
